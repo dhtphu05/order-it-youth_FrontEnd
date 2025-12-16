462a59517f8b0f1a2cb997bc11f774573632bfcc
Đoàn Hoàng Thiên Phú <dhtphu05@gmail.com>
2025-12-17 00:18:42 +0700

    chore: hide order log

--- app/page.tsx
+++ app/page.tsx
@@ -22,7 +22,7 @@ export default function Home() {
 
       <Activities />
       {/* <Transparency /> */}
-      <OrdersLog />
+      {/* <OrdersLog /> */}
       {/* <Marketplace /> */}
       {/* <Gallery /> */}
       <Footer />
